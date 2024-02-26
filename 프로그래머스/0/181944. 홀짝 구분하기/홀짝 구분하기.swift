b0df33b73325cc50f50c0361a6bf1c925eeab0a5
CHACHA <chajanee@gmail.com>
2024-02-26 14:43:33 +0900

    [level 0] Title: 홀짝 구분하기, Time: 7.37 ms, Memory: 12.8 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.swift
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.swift
@@ -2,8 +2,4 @@ import Foundation
 
 let a = Int(readLine()!)!
 
-let result = a % 2 == 0
-
-// print(result)
-
-print(result ? "\(a) is even" : "\(a) is odd")
+print(a % 2 == 0 ? "\(a) is even" : "\(a) is odd")
